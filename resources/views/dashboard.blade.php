46bfb4832376cec5eec72050ae72fd186fb5f513
Riley-Brown <riley@riley.gg>
2019-02-02 00:05:28 -0500

    removed container div and placed it in app template file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     <h3>Your Listings</h3>
                    @if(count($listings))
@@ -32,5 +31,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection

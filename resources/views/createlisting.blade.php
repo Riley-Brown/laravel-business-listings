797aac9645b82207eaee737690e0a19bb0d364f2
Riley-Brown <riley@riley.gg>
2019-02-03 00:19:13 -0500

    Adds back button and removed extra name form field

--- resources/views/createlisting.blade.php
+++ resources/views/createlisting.blade.php
@@ -5,7 +5,9 @@
 <div class="row justify-content-center">
     <div class="col-md-8">
         <div class="card">
-            <div class="card-header">Create Listing</div>
+            <div class="card-header">Create Listing
+                <a href="/dashboard" class="float-right btn btn-secondary btn-sm">Go Back</a>
+            </div>
             <div class="card-body">
               {!!Form::open(['action' => 'ListingsController@store', 'method' => 'POST'])!!}
                 {{Form::bsText('name', '', ['placeholder' => 'Company Name'])}}
@@ -14,7 +16,6 @@
                 {{Form::bsText('phone', '', ['placeholder' => 'Company Phone'])}}
                 {{Form::bsText('address', '', ['placeholder' => 'Business Address'])}}
                 {{Form::bsTextArea('bio', '', ['placeholder' => 'Company Bio'])}}
-                {{Form::bsText('name', '', ['placeholder' => 'Company Name'])}}
                 {{Form::bsSubmit('submit')}}
               {!! Form::close() !!}
                

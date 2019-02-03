40b45477d8757c323a1f41180aca826a11c6746a
Riley-Brown <riley@riley.gg>
2019-02-02 23:26:44 -0500

    Added button to dashboard to create listing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,38 @@
-@extends('layouts.app')
-
+@extends('layouts.app') 
 @section('content')
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                <div class="card-body">
-                    <h3>Your Listings</h3>
-                   @if(count($listings))
-                    <table class="table table-striped">
-                        <tr>
-                            <th>Company</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        @forEach($listings as $listing)
-                        <tr>
-                            <td>{{$listing->name}}</td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        @endforeach
-                    </table>
-                  
-                   @endif
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">
+                Dashboard
+                <span class="float-right">
+                    <a href="/listings/create" class="btn btn-success btn-sm">Create Listing</a>
+                </span>
+            </div>
+            <div class="card-body">
+                <h3>Your Listings</h3>
+                @if(count($listings))
+                <table class="table table-striped">
+                    <tr>
+                        <th>Company</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    @forEach($listings as $listing)
+                    <tr>
+                        <td>{{$listing->name}}</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    @endforeach
+                </table>
 
-                   
-                </div>
+                @endif
+
+
             </div>
         </div>
     </div>
+</div>
 @endsection

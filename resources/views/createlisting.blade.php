2587530114dd792f7fbe9712ae50b6aea11b3b78
Riley-Brown <riley@riley.gg>
2019-02-02 23:53:27 -0500

    Added website form input

--- resources/views/createlisting.blade.php
+++ resources/views/createlisting.blade.php
@@ -9,6 +9,7 @@
             <div class="card-body">
               {!!Form::open(['action' => 'ListingsController@store', 'method' => 'POST'])!!}
                 {{Form::bsText('name', '', ['placeholder' => 'Company Name'])}}
+                {{Form::bsText('website', '', ['placeholder' => 'Company Website'])}}
                 {{Form::bsText('email', '', ['placeholder' => 'Company Email'])}}
                 {{Form::bsText('phone', '', ['placeholder' => 'Company Phone'])}}
                 {{Form::bsText('address', '', ['placeholder' => 'Business Address'])}}

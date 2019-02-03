c4dbd2e03e883c145e7d810deebd8391ad1337ac
Riley-Brown <riley@riley.gg>
2019-02-03 01:28:10 -0500

    added form submit component to initiate delete for listing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,8 +22,15 @@
                     @forEach($listings as $listing)
                     <tr>
                         <td>{{$listing->name}}</td>
-                        <td><a class="btn btn-secondary float-right" href="/listings/{{$listing->id}}/edit">Edit</a></td>
-                        <td></td>
+                        <td>
+                            <a class="btn btn-secondary float-right" href="/listings/{{$listing->id}}/edit">Edit</a>
+                        </td>
+                        <td>
+                            {!!Form::open(['action' => ['ListingsController@destroy', $listing->id], 'method' => 'POST', 'class' => 'float-right', 'onsubmit' => 'return confirm("Are you sure?")'])!!}
+                                {{Form::hidden('_method', 'DELETE')}}
+                                {{Form::bsSubmit('Delete', ['class' => 'btn btn-danger'])}}
+                            {!! Form::close() !!}
+                        </td>
                     </tr>
                     @endforeach
                 </table>
